b642f96a1a1b1af9de5fd05f71b3f59021568403
ssang21 <skim145@liberty.edu>
2025-04-14 14:39:16 -0400

    Post Order

--- BST.h
+++ BST.h
@@ -33,6 +33,7 @@ private:
   E* findhelp(BSTNode<Key, E>*, const Key&) const;
   void printhelp(BSTNode<Key, E>*, int) const;
   void visit(BSTNode<Key, E>*) const;
+  void printPostHelp(BSTNode<Key, E>*, int) const;
 
 public:
   BST() { root = NULL; nodecount = 0; }  // Constructor
@@ -90,6 +91,11 @@ public:
   void print() const { // Print the contents of the BST
     if (root == NULL) cout << "The BST is empty.\n";
     else printhelp(root, 0);
+  }
+
+  void printPostOrder() const {
+    if (root == NULL) cout << "The BST is empty. \n";
+    else printPostHelp()
   }
   
 };
@@ -196,4 +202,13 @@ printhelp(BSTNode<Key, E>* root, int level) const {
   printhelp(root->left(), level+1);   // Do left subtree
   visit(root);						  // Print node value
   printhelp(root->right(), level+1);  // Do right subtree
+}
+
+template <typename Key, typename E>
+void BST<Key, E>::
+printPostHelp(BSTNode<Key, E>* root, int level) const {
+  if (root == NULL) return;           // Empty tree
+  printhelp(root->left(), level+1);   // Do left subtree
+  printhelp(root->right(), level+1);  // Do right subtree
+  visit(root);						  // Print node value
 }
